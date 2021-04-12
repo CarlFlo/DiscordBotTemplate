56fc395eb17b88330d7b392cc1a442923a3b096f
CarlFlo <Carl.flodin@hotmail.com>
2021-04-13 00:10:32 +0200

    Added initial work for supporting ping with timing

--- bot/commands/ping.go
+++ bot/commands/ping.go
@@ -11,4 +11,24 @@ import (
 func Ping(s *discordgo.Session, m *discordgo.MessageCreate, input structs.CmdInput) {
 
 	cmdutils.SendDirectMessage(s, m, "Pong!")
+
+	/*
+		// Send ping
+		pingMsg, err := cmdutils.SendDirectMessage(s, m, "Pinging...")
+		if err != nil {
+			log.Printf("Error: %s", err)
+			return
+		}
+
+		// Update message
+		s.ChannelMessageEdit(pingMsg.ChannelID, pingMsg.ID, "Pinging... :bar_chart:")
+
+		// Save this and pass it so the messageUpdateHandler chan access it
+		// pingMsg.ID
+		// discordgo.SnowflakeTimestamp(pingMsg.ID)
+
+		// elapsed := time.Since(initalTime)
+		// This info will we used to calculate the ping
+		// Send the ID, match the ID and use the cached timestamp to compare
+	*/
 }
---
+++ bot/messageUpdateHandler.go
@@ -0,0 +1,9 @@
+package bot
+
+import (
+	"github.com/bwmarrin/discordgo"
+)
+
+func messageUpdateHandler(s *discordgo.Session, mu *discordgo.MessageUpdate) {
+
+}
